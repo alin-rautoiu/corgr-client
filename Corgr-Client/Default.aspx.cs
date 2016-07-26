248ad88a11c4f380d159d6e3edf8552d2d20ff40
alin.rautoiu <alin.rautoiu@teamnet.ro>
2016-07-26 14:19:36 +0300

    Am mai bagat o fisa la IoC.

--- Corgr-Client/Default.aspx.cs
+++ Corgr-Client/Default.aspx.cs
@@ -6,18 +6,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Autofac.Integration.Web;
+using Autofac;
 
 namespace Corgr_Client
 {
     public partial class _Default : Page
     {
-        public IDogsInTheSkyService dogsIntheSkyService { get; set; }
+        public IDogsInTheSkyService DogsIntheSkyService { get; set; }
+
+        protected override void OnPreInit(EventArgs e)
+        {
+            var cpa = (IContainerProviderAccessor)HttpContext.Current.ApplicationInstance;
+            var cp = cpa.ContainerProvider;
+            cp.RequestLifetime.InjectProperties(this); 
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            dogsIntheSkyService = new DogsInTheSkyService();
-
-            var Dog1 = dogsIntheSkyService.GetRandomDog();
+            var Dog1 = DogsIntheSkyService.GetRandomDog();
           
             DogImage.ImageUrl = Dog1.Face;
             DogName.Text = Dog1.Name;
